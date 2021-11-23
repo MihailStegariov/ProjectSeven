e9b2c87f64fe8956ed040f5e9907822a26910378
Mihail <Mihail@LAPTOP-DTKS6POR.fios-router.home>
2021-11-22 22:16:39 -0500

    Welcome to GitHub ;)

---
+++ src/project/seven/Seven.java
@@ -0,0 +1,13 @@
+package project.seven;
+
+public class Seven {
+
+	public static void main(String[] args) {
+		
+		int i = 0;
+		int b = 10;
+		int result = i+b;
+		System.out.println(result);
+		
+	}
+}
